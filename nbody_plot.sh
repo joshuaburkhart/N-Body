da6e148462e16c5f83528b04932078d26f441dc7
Joshua Burkhart <jburkhar@hn1.(none)>
2012-02-12 12:48:24 -0800

    added R visualization script

---
+++ nbody_plot.sh
@@ -0,0 +1,13 @@
+R --no-save << EOT
+A = read.table("$1")
+par(pty="s")
+m = max(A)
+plot(c(-m,m),c(-m,m),xlab="X",ylab="Y",main="N Body Project",pch=" ")
+points(A[,2],A[,3],col="red")
+points(A[,11],A[,12],col="red")
+points(A[,14],A[,15],col="red")
+points(A[,17],A[,18],col="red")
+points(A[,20],A[,21],col="red")
+quit("no")
+EOT
+evince "./Rplots.pdf"
