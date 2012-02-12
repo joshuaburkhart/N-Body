2098336c2fb1c6f443fbf7a5691a50cd4e17f522
Joshua Burkhart <jburkhar@hn1.(none)>
2012-02-11 18:21:12 -0800

    working for 2 timesteps

--- mpinbody_prot.C
+++ mpinbody_prot.C
@@ -17,7 +17,6 @@
 */
 
 #include <math.h>
-using std:sqrt;
 
 #include <stdlib.h>
 #include <fstream>
@@ -36,9 +35,10 @@ using std::string;
 #include <mpi.h>
 
 const int DT = 86459;     // time step = number of seconds in one day
-const int T = 365;        // number of time steps to execute
+const int T = 2;//365;        // number of time steps to execute
+const double G = 6.67E-11;
 
-const int NP = sizeof(b)/sizeof(Body);
+//const int NP = sizeof(b)/sizeof(Body);
 
 Vector calc_accel(Body i, double x, double y, double z, double jmass);
 int output(int t, int h, int w, double buf[]);
@@ -93,22 +93,22 @@ MPI_Init(&argc,&argv);
     if(input_data.good()){
       input_data >> total_bod_count;
       bods_per_proc = total_bod_count/nprocs;
-      printf("total_bod_count is %i!\n",total_bod_count);
-      printf("bods_per_proc is %i!\n",bods_per_proc);
+      //printf("total_bod_count is %i!\n",total_bod_count);
+      //printf("bods_per_proc is %i!\n",bods_per_proc);
     }
     local_bod_array = (double*) malloc(sizeof(double) * 7 * bods_per_proc);
     getline(input_data,line);//moving to the first row of data
     while(input_data.good() && row < total_bod_count){
       getline(input_data,line);
       if(row==line_number){
-        printf("I am thread %i at row %i, storing body %i\n",myid,row,local_bod_idx);
+        //printf("I am thread %i at row %i, storing body %i\n",myid,row,local_bod_idx);
         size_t start = 0;
         size_t end = 0;
         for(int j=0;j<7;j++){
           end = line.find(" ",start+1);
           double dub = atof(line.substr(start,end).c_str());
           *(local_bod_array+(local_bod_idx*7)+j) = dub;
-          printf("start: %lu, end: %lu, just added measurement %f\n",start,end,dub);
+          //printf("start: %lu, end: %lu, just added measurement %f\n",start,end,dub);
           start = end;
         }
         row+=nprocs;
@@ -118,19 +118,19 @@ MPI_Init(&argc,&argv);
     }
   }
 local_bod_count = local_bod_idx;//it's a better name for the var from here on down
-printf("Thread %i holding %i bodies\n",myid,local_bod_count);
+//printf("Thread %i holding %i bodies\n",myid,local_bod_count);
 for(int i = 0; i < local_bod_count; i++){
-printf("bod %i mass: %f\n",i,*(local_bod_array+(i*7)+0));
-printf("bod %i x: %f\n",i,*(local_bod_array+(i*7)+1));
-printf("bod %i y: %f\n",i,*(local_bod_array+(i*7)+2));
-printf("bod %i z: %f\n",i,*(local_bod_array+(i*7)+3));
-printf("bod %i vx: %f\n",i,*(local_bod_array+(i*7)+4));
-printf("bod %i vy: %f\n",i,*(local_bod_array+(i*7)+5));
-printf("bod %i vz: %f\n",i,*(local_bod_array+(i*7)+6));
+//printf("bod %i mass: %f\n",i,*(local_bod_array+(i*7)+0));
+//printf("bod %i x: %f\n",i,*(local_bod_array+(i*7)+1));
+//printf("bod %i y: %f\n",i,*(local_bod_array+(i*7)+2));
+//printf("bod %i z: %f\n",i,*(local_bod_array+(i*7)+3));
+//printf("bod %i vx: %f\n",i,*(local_bod_array+(i*7)+4));
+//printf("bod %i vy: %f\n",i,*(local_bod_array+(i*7)+5));
+//printf("bod %i vz: %f\n",i,*(local_bod_array+(i*7)+6));
 }
 
 int sendbuf_size=(4 * bods_per_proc) + 1;
-printf("sendbuf_size is %i\n",sendbuf_size);
+//printf("sendbuf_size is %i\n",sendbuf_size);
 
 double *sendbuf,*recvbuf;
 sendbuf = (double*) malloc(sizeof(double) * sendbuf_size);
@@ -139,6 +139,9 @@ recvbuf = (double*) malloc(sizeof(double) * sendbuf_size * nprocs); //different 
 *(sendbuf)= (double) local_bod_count;
 
 for(int t=0;t<T;t++){
+  if(myid==0){
+    cout << t << " ";
+  }
   //sendbuf arrangement: 0->local_bod_count, 1->bod0.m, 2->bod0.x, 3->bod0.y, 4->bod0.z, 5->bod1.m, ... , (4n+1)->bodn.m, (4n+2)->bodn.x, (4n+3)->bodn.y, (4n+4)->bodn.z
   for(int i = 0; i < local_bod_count; i++){
     *(sendbuf+(i*4)+1)=*(local_bod_array+(i*7)+0);
@@ -147,65 +150,103 @@ for(int t=0;t<T;t++){
     *(sendbuf+(i*4)+4)=*(local_bod_array+(i*7)+3);
   }
 
-  printf("local_bod_count is %i\n",local_bod_count);
+  //printf("local_bod_count is %i\n",local_bod_count);
 
   for(int i = 0; i < sendbuf_size; i++){
-    printf("sendbuf %i is %f\n",i,*(sendbuf+i));
+    //printf("sendbuf %i is %f\n",i,*(sendbuf+i));
   }
 
   MPI_Allgather(sendbuf,sendbuf_size,MPI_DOUBLE,recvbuf,sendbuf_size,MPI_DOUBLE,MPI_COMM_WORLD);
   
-  printf("Listing all bod counts\n");
+  //printf("Listing all bod counts\n");
   for(int i=0; i<nprocs;i++){
-    if(i != myid){//skip my own bodies
-      int foreign_bod_count=(int)  *(recvbuf+(i * sendbuf_size));
-      printf("sendbuf bod count %i is %f\n",i,foreign_bod_count);
+    int foreign_bod_count=(int)  *(recvbuf+(i * sendbuf_size));
+    //printf("sendbuf bod count %i is %f\n",i,foreign_bod_count);
+    for(int k = 0; k < local_bod_count; k++){
+      double local_x;
+      double local_y;
+      double local_z;
+      double local_vx;
+      double local_vy;
+      double local_vz;
+      double accel_x=0;
+      double accel_y=0;
+      double accel_z=0;
       for(int j = 0; j < foreign_bod_count; j++){
-        double foreign_m = *(recvbuf+(i * sendbuf_size)+(4 * j)+1);
-        double foreign_x = *(recvbuf+(i * sendbuf_size)+(4 * j)+2);
-        double foreign_y = *(recvbuf+(i * sendbuf_size)+(4 * j)+3);
-        double foreign_z = *(recvbuf+(i * sendbuf_size)+(4 * j)+4);
-        for(int k = 0; k < local_bod_count; k++){
+        if(!(i == myid && j==k)){//skip bodies that are themselves
+          double foreign_m = *(recvbuf+(i * sendbuf_size)+(4 * j)+1);
+          double foreign_x = *(recvbuf+(i * sendbuf_size)+(4 * j)+2);
+          double foreign_y = *(recvbuf+(i * sendbuf_size)+(4 * j)+3);
+          double foreign_z = *(recvbuf+(i * sendbuf_size)+(4 * j)+4);
           double local_m = *(local_bod_array+(k*7)+0);
-          double local_x = *(local_bod_array+(k*7)+1);
-          double local_y = *(local_bod_array+(k*7)+2);
-          double local_z = *(local_bod_array+(k*7)+3);
-          double local_vx = *(local_bod_array+(k*7)+4);
-          double local_vy = *(local_bod_array+(k*7)+5);
-          double local_vz = *(local_bod_array+(k*7)+6);
+          local_x = *(local_bod_array+(k*7)+1);
+          local_y = *(local_bod_array+(k*7)+2);
+          local_z = *(local_bod_array+(k*7)+3);
+          local_vx = *(local_bod_array+(k*7)+4);
+          local_vy = *(local_bod_array+(k*7)+5);
+          local_vz = *(local_bod_array+(k*7)+6);
           // Vector k = i.position() - j;
           double dx = local_x - foreign_x;
           double dy = local_y - foreign_y;
           double dz = local_z - foreign_z;
           // double l = (k * (k * k)).norm();
           double dot_p = dx*dx + dy*dy + dz*dz;
-          dx *= dot_p;
-          dy *= dot_p;
-          dz *= dot_p;
-          double l = sqrt(dx*dx + dy*dy + dz*dz);
+          double tdx = dx * dot_p;
+          double tdy = dy * dot_p;
+          double tdz = dz * dot_p;
+          double l = sqrt(tdx*tdx + tdy*tdy + tdz*tdz);
           // Vector m = (k * ((double) 1/l)) * -1 * G * jmass;
-          double accel_x = dx * ((double) 1/l) * -1 * foreign_m;
-          double accel_y = dy * ((double) 1/l) * -1 * foreign_m;
-          double accel_z = dz * ((double) 1/l) * -1 * foreign_m;
-          // Vector newVeloc = b[myid].velocity() += accel * DT;
-          double new_vx = local_vx += accel_x * DT;
-          double new_vy = local_vy += accel_y * DT;
-          double new_vz = local_vz += accel_z * DT;
-          // Vector newPosit = b[myid].position() += newVeloc * DT;
-          double new_x = local_x += new_vx * DT;
-          double new_y = local_y += new_vy * DT;         
-          double new_z = local_z += new_vz * DT;
-          // b[myid].setPosition(newPosit);
-          *(local_bod_array+(k*7)+1) = new_x;
-          *(local_bod_array+(k*7)+2) = new_y;
-          *(local_bod_array+(k*7)+3) = new_z;
-          // b[myid].setVelocity(newVeloc);
-          *(local_bod_array+(k*7)+4) = new_vx;
-          *(local_bod_array+(k*7)+5) = new_vy;
-          *(local_bod_array+(k*7)+6) = new_vz;
+          double tax = dx * ((double) 1/l) * -1 * G * foreign_m;
+          double tay = dy * ((double) 1/l) * -1 * G * foreign_m;
+          double taz = dz * ((double) 1/l) * -1 * G * foreign_m;
+          accel_x += tax;
+          accel_y += tay;
+          accel_z += taz;
+          //cout << "local coords: " << local_x << " " << local_y << " " << local_z << endl;
+          //cout << "foreign coords: " << foreign_x << " " << foreign_y << " " << foreign_z << endl;
+          //cout << "k is " << dx << " " << dy << " " << dz << endl;
+          //cout << "l is " << l << endl;
+          //cout << "m is " << tax << " " << tay << " " << taz << endl;
+          //printf("mass of foreign body %i and local body %i\n",j,k);          
+          //cout << foreign_m << endl << local_m << endl;          
+          //printf("accelleration vector from foreign body %i to local body %i is ",j,k);
+          //cout << accel_x << " " << accel_y << " " << accel_z << " " << endl;
         }   
       }
+      // Vector newVeloc = b[myid].velocity() += accel * DT;
+      double new_vx = local_vx += accel_x * DT;
+      double new_vy = local_vy += accel_y * DT;
+      double new_vz = local_vz += accel_z * DT;
+      // Vector newPosit = b[myid].position() += newVeloc * DT;
+      double new_x = local_x += new_vx * DT;
+      double new_y = local_y += new_vy * DT;         
+      double new_z = local_z += new_vz * DT;
+      // b[myid].setPosition(newPosit);
+      *(local_bod_array+(k*7)+1) = new_x;
+      *(local_bod_array+(k*7)+2) = new_y;
+      *(local_bod_array+(k*7)+3) = new_z;
+      // b[myid].setVelocity(newVeloc);
+      *(local_bod_array+(k*7)+4) = new_vx;
+      *(local_bod_array+(k*7)+5) = new_vy;
+      *(local_bod_array+(k*7)+6) = new_vz;
     }
+  }
+  //print coords for this timestep
+  if(myid==0){
+    for(int i = 0;i < bods_per_proc; i++){
+      for(int j = 0; j < nprocs; j++){
+        int tmp_bod_count=(int) *(recvbuf+(j * sendbuf_size)+0);
+        if(i < tmp_bod_count){
+          double tmp_x = *(recvbuf+(j * sendbuf_size)+(i * 4)+2);
+          double tmp_y = *(recvbuf+(j * sendbuf_size)+(i * 4)+3);
+          double tmp_z = *(recvbuf+(j * sendbuf_size)+(i * 4)+4);
+          //printf("%f %f %f ",tmp_x,tmp_y,tmp_z);
+          cout << tmp_x << " " << tmp_y << " " << tmp_z << " ";
+        }
+      }
+    }
+    //printf("\n");
+    cout << endl;
   }
 }
   
@@ -252,7 +293,7 @@ for(int t=0;t<T;t++){
 */
   free(sendbuf);
   free(recvbuf);
-  free(bod_array);
+  free(local_bod_array);
   return MPI_Finalize();
 }
 
